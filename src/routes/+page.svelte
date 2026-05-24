21aead3c6c8731e8c227e8fc8b00b2bffb15e664
bluekiwidev <111331826+bluekiwidev@users.noreply.github.com>
2026-05-24 02:30:54 +0000

    Added styling

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -7,10 +7,12 @@
 </style>
 
 <ul>
-  <img src={logo} alt="Notely logo" class="logo" />
+  <li><img src={logo} alt="Notely logo" class="logo"></li>
   <li><a href="default.asp">Home</a></li>
   <li><a href="news.asp">News</a></li>
   <li><a href="contact.asp">Contact</a></li>
   <li><a href="about.asp">About</a></li>
+  <li><a href="/signup">Sign Up</a></li>
+  <li><a href="/signin">Sign In</a></li>
 </ul>
 
