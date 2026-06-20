a7fc84f63fbad92998405f6d03a84988e590e8a6
Beijing-corn87 <111331826+bluekiwidev@users.noreply.github.com>
2026-06-20 17:47:25 +1200

    Added tailwind

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,15 +1,9 @@
 <script lang="ts">
-  import favicon from "$lib/assets/favicon.svg";
+	import './layout.css';
+	import favicon from "$lib/assets/favicon.svg";
 
-  let { children } = $props();
+	let { children } = $props();
 </script>
 
-<style>
-    @import "$lib/assets/style.css";
-</style>
-
-<svelte:head>
-  <link rel="icon" href={favicon} />
-</svelte:head>
-
+<svelte:head><link rel="icon" href={favicon} /></svelte:head>
 {@render children()}
